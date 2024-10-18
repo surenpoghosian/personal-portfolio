e81d943a17e4cce229d2fb68008ae4bf4ac26988
Suren Poghosyan <suren.poghosian.18@gmail.com>
2024-10-18 13:26:47 +0400

    [main] fix minor eslint issues

--- src/components/shared/SocialLinks.tsx
+++ src/components/shared/SocialLinks.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin';
-import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter';
-import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram';
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub';
 import { FaStackOverflow } from '@react-icons/all-files/fa/FaStackOverflow';
 import { FaMedium } from '@react-icons/all-files/fa/FaMedium';
-import { FaFacebookSquare } from '@react-icons/all-files/fa/FaFacebookSquare';
 import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube';
-import { FaDev } from '@react-icons/all-files/fa/FaDev';
 
 import { socialLinkTypes } from '../../types/SocialLink';
 import type { SocialLink as SocialLinkType } from '../../types/SocialLink';
@@ -22,30 +18,23 @@ type SocialLinksProps = {
 };
 
 const linkToIcon = {
-  [socialLinkTypes.twitter]: FaTwitter,
-  [socialLinkTypes.instagram]: FaInstagram,
   [socialLinkTypes.gitHub]: FaGithub,
   [socialLinkTypes.stackOverflow]: FaStackOverflow,
   [socialLinkTypes.linkedIn]: FaLinkedin,
   [socialLinkTypes.medium]: FaMedium,
-  [socialLinkTypes.facebook]: FaFacebookSquare,
-  [socialLinkTypes.devTo]: FaDev,
   [socialLinkTypes.youTube]: FaYoutube,
 };
 
 const SocialLinks = (props: SocialLinksProps): React.ReactElement | null => {
-  const {
-    links = [],
-    expandable = true,
-    iconClassName = 'w-6 h-6',
-    itemClassName = 'mr-4 mb-2',
-  } = props;
+  // eslint-disable-next-line object-curly-newline
+  const { links = [], expandable = true, iconClassName = 'w-6 h-6', itemClassName = 'mr-4 mb-2' } = props;
 
   if (!links) {
     return null;
   }
 
-  const filteredLinks = links.filter((link) => !link?.hidden);
+  // eslint-disable-next-line arrow-parens
+  const filteredLinks = links.filter(link => !link?.hidden);
 
   const onRender = (socialLink: SocialLinkType): React.ReactElement => {
     let linkIcon = null;
@@ -54,11 +43,7 @@ const SocialLinks = (props: SocialLinksProps): React.ReactElement | null => {
       const Icon = linkToIcon[linkType];
       linkIcon = <Icon className={iconClassName} />;
     }
-    return (
-      <HyperLink link={socialLink}>
-        {linkIcon || socialLink.url}
-      </HyperLink>
-    );
+    return <HyperLink link={socialLink}>{linkIcon || socialLink.url}</HyperLink>;
   };
 
   const toHide = (socialLink: SocialLinkType): boolean => {
