94a1609cdb17f697d8d877e609fd2246136d20e4
Suren Poghosyan <suren.poghosian.18@gmail.com>
2024-10-22 11:23:02 +0400

    [main] update about me section

--- src/components/shared/Greeting.tsx
+++ src/components/shared/Greeting.tsx
@@ -37,12 +37,6 @@ const Greeting = (): React.ReactElement => {
       I’ve been exploring distributed systems with Raspberry Pi clusters and diving into AI with Jetson Orin Nano. In my
       free time, I contribute to open-source and write {blogLinkElement}, sharing thoughts on coding, design patterns,
       and tech.
-      {/* Hi there! I’m Suren. I’m a software enthusiast deeply involved in systems design and development. I spend most of
-      my time working with Node.js, TypeScript, and Python, creating systems that emphasize scalability and efficiency.
-      I enjoy exploring software architecture patterns like Creational, Structural, and Behavioral, and implementing
-      them to solve real-world problems. In my spare time, I contribute to open-source projects, helping others learn
-      about algorithms, clean code, and backend development. I also write {blogLinkElement} about software engineering,
-      sharing my insights on design patterns, coding practices, and personal development in tech. */}
     </p>
   );
 };
